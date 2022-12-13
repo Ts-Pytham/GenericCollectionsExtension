de85d8e0b37cefcf3f86436a66425328422eb31c
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-12-12 21:31:31 -0500

    Update website DocFX

--- GenericCollectionsExtension.Tests/Usings.cs
+++
@@ -1,5 +0,0 @@
-global using GenericCollectionsExtension.Exceptions;
-global using GenericCollectionsExtension.Queue;
-global using GenericCollectionsExtension.SortedList;
-global using GenericCollectionsExtension.Tree;
-global using Xunit;
