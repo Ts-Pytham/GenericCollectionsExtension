7a3cbe2b23bd39efd9712beb0f729c668ba6c0ab
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-12-31 12:26:39 -0500

    Created Graph interface

---
+++ GenericCollectionsExtension/Graph/IGraph.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCollectionsExtension.Graph
+{
+    public interface IGraph<TVertex, TEdge>
+        where TEdge : IComparable<TEdge>
+    {
+        public void AddVertex(TVertex vertex);
+
+        public void AddEdge(TVertex v1, TVertex v2, TEdge edge);
+
+        public void RemoveVertex(TVertex vertex);
+
+        public void RemoveEdge(TVertex v1, TVertex v2);
+
+        public bool HasVertex(TVertex vertex);
+
+        public bool HasEdge(TVertex v1, TVertex v2);
+
+    }
+}
