6d6f445b2c8368a8056f4e73d0923281501c978a
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2022-12-31 12:36:27 -0500

    Created Edge and Vertex

---
+++ GenericCollectionsExtension/Graph/Edge.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCollectionsExtension.Graph
+{
+    internal class Edge<T, U>
+        where U : IComparable<U>
+    {
+        public T Suceessor { get; set; }
+
+        public U Cost { get; set; }
+
+        public Edge(T suceessor, U cost)
+        {
+            Suceessor = suceessor;
+            Cost = cost;
+        }
+    }
+}
---
+++ GenericCollectionsExtension/Graph/Vertex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericCollectionsExtension.Graph
+{
+    internal class Vertex<T, U>
+        where U : IComparable<U>
+    {
+        public T VertexName { get; set; }
+
+        public List<Edge<T, U>> Edges { get; }
+
+        public Vertex(T Vertex)
+        {
+            VertexName = Vertex;
+        }
+
+        public void AddEdge(Edge<T, U> edge)
+        {
+            Edges.Add(edge);
+        }
+
+    }
+}
